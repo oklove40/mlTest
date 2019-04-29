718f0097195aba1256a62195d40a40b4b4d28cf8
김대진 <oklove40@hotmail.com>
2019-04-29 18:00:44 +0900

    회귀 학습자를 사용하여 가격 예측 테스트중

---
+++ 03.Test/mlTest.Test.TaxiFarePrediction/mlTest.Test.TaxiFarePrediction/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.IO;
+using Microsoft.ML;
+
+namespace mlTest.Test.TaxiFarePrediction
+{
+    class Program
+    {
+        static readonly string _trainDataPath = Path.Combine(Environment.CurrentDirectory, "Data", "taxi-fare-train.csv");
+        static readonly string _testDataPath = Path.Combine(Environment.CurrentDirectory, "Data", "taxi-fare-test.csv");
+        static readonly string _modelPath = Path.Combine(Environment.CurrentDirectory, "Data", "Model.zip");
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ 03.Test/mlTest.Test.TaxiFarePrediction/mlTest.Test.TaxiFarePrediction/TaxiTrip.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.ML.Data;
+
+namespace mlTest.Test.TaxiFarePrediction
+{
+    public class TaxiTrip
+    {
+        /*
+         vendor_id,rate_code,passenger_count,trip_time_in_secs,trip_distance,payment_type,fare_amount
+             */
+        [LoadColumn(0)]
+        public string VendorId;
+        [LoadColumn(1)]
+        public string RateCode;
+        [LoadColumn(2)]
+        public string PassengerCount;
+        [LoadColumn(3)]
+        public string Triptime;
+        [LoadColumn(4)]
+        public string TripDistance;
+        [LoadColumn(5)]
+        public string PaymentType;
+        [LoadColumn(6)]
+        public string FareAmount;
+    }
+
+    public class TaxiTripFarePrediction
+    {
+        [ColumnName("Score")]
+        public float FareAmount;
+    }
+}
